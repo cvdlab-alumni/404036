cefcf8897d1b53c93a1a900b8c6680a564a97c49
Daniele <conti.daniele88@gmail.com>
2014-05-20 18:31:48 +0200

    es1 mantengo celle

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -2,16 +2,33 @@ from larcc import *
 
 DRAW = COMP([VIEW,STRUCT,MKPOLS])
 
+def sottrai(lista1, lista2):
+	lst=[]
+	for e in lista1:
+		if e not in lista2:
+			lst.append(e)
+	return lst
+
+
+
 def dis(pol):
 	hpc=SKEL_1(STRUCT(MKPOLS(pol)))
 	hpc = cellNumbering (pol,hpc)(range(len(pol[1])),CYAN,1)
 	return hpc
 
-def rem(list, pol):
-	toRemove = list
-	V,CV = pol
-	pol = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-	return pol
+""" rimuove una cella che va persa """
+# def rem(list, pol):
+# 	toRemove = list
+# 	V,CV = pol
+# 	pol = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+# 	return pol
+
+""" rimuove una cella e la inserisce in una lista """
+# def rem(toRemove, pol, addTo):
+# 	V,CV=pol
+# 	pol = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+# 	addTo=sottrai(CV,pol[1])
+# 	return (pol,addTo )
 
 def unisci(num,list1, list2, pol):
 	merge=num
@@ -19,44 +36,35 @@ def unisci(num,list1, list2, pol):
 	pol_final = diagram2cell(room,pol,merge)
 	return pol_final
 
+def select(list, pol):
+	lst=[]
+	for i in list:
+		lst.append(pol[1][i])
+	return lst
+
+
+""" ********************************************************************************************************************************************** """
 
 house=assemblyDiagramInit([5,3,2])([[.3,5,.3,5.0,.3],[.3,10,.3],[.3,3]])
 
+""" liste celle utili """
 V,CV = house
-
-""" Creo il salone """
-hpc_house=dis(house)
-# VIEW(hpc_house)
-
-""" Identifico rimuovo il blocco del salone"""
-
-house= rem([9],house)
-# DRAW(house)
 
 """ Rinumero el celle """
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-""" Creo stanze piano terra destra """
-stanze=assemblyDiagramInit([3,3,1])([[2.5,0.3,2.7],[3.5,6.5],[1]])
-
-""" Mostro il blocco in cui innesto """
-toMerge=20
-# solidifico la stanza toMerge per vederla
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
+toMerge=21
 
 """ Creo nuove stanze e innesto"""
-stanze = assemblyDiagramInit([3,1,1])([[2.7,0.2,2.5],[.3],[2.2]])
+stanze = assemblyDiagramInit([3,1,1])([[2.5,0.2,2.3],[.3],[2.2]])
 house = diagram2cell(stanze,house,toMerge)
 
-#rinumero
+
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-toMerge=30
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
+toMerge=31
 
 """ Camera, Bagno, Camera """
 stanze = assemblyDiagramInit([3,5,1])([[1,0.3,4],[3,.2,3,.2,3],[2.2]])
@@ -64,16 +72,7 @@ house = diagram2cell(stanze,house,toMerge)
 
 hpc_house=dis(house)
 # VIEW(hpc_house)
-
-house= rem([40,42,44,30,35,32,33,34,39], house)
-# DRAW(house)
-
-hpc_house=dis(house)
-# VIEW(hpc_house)
-
-toMerge=28
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
+toMerge=29
 
 """ Bagno, Cucina, ingresso """
 stanze = assemblyDiagramInit([1,5,1])([[5],[3,.2,3.5,.2,2.5],[2.2]])
@@ -82,49 +81,73 @@ house = diagram2cell(stanze,house,toMerge)
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-house=rem([35,37,39], house)
-# DRAW(house)
 
-""" porta"""
-toMerge=18
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
+""" porte """
+toMerge=19
+# cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
 # VIEW(STRUCT([hpc_house,cell]))
 
 house=unisci(toMerge,[3,1,1], [[14,1.1,13],[1],[1]], house)
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-toMerge=36
-# # solidifico la stanza toMerge per vederla
-# cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
+toMerge=49
+# # # solidifico la stanza toMerge per vederla
+# # cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
+# # VIEW(STRUCT([hpc_house,cell]))
+""" porta principale """
+house = unisci(toMerge,[3,1,2],[[0.75,1,0.75],[.3],[2,1]],house)
 
-house = unisci(36,[3,1,2],[[4,11,4],[.3],[2.2,.5]],house)
-hpc_house=dis(house)
-# VIEW(hpc_house)
+# house= rem([40],house)
+# # DRAW(house)
+""" porta ingresso/cucina """
+house = unisci(45,[3,1,2],[[0.75,1,0.75],[.3],[2,1]],house)
+# hpc_house=dis(house)
+""" porta ingresso/salone """
+house = unisci(15,[1,4,2],[[.3],[1,1,1,6.4],[2,1]],house)
+# hpc_house=dis(house)
+""" porta /ingresso/camera"""
+house = unisci(27,[1,4,2],[[.3],[1,1,1,6.4],[2,1]],house)
+# hpc_house=dis(house)
+""" porta camera1/bagno """
+house = unisci(38,[3,1,2],[[0.7,1.5,0.7],[1],[2,1]],house)
+# hpc_house=dis(house)
+""" Divisione parete camera cucina"""
+house = unisci(73,[1,2,1],[[1],[1,1],[1]],house)
+# hpc_house=dis(house)
+house = unisci(80,[1,3,1],[[1],[0.7,0.8,1.3],[1]],house)
+# hpc_house=dis(house)
+house = unisci(80,[1,3,1],[[1],[1.2,0.7,0.8],[1]],house)
+# hpc_house=dis(house)
+""" Finestre """
+house = unisci(3,[1,9,3],[[1],[0.7,2.4,0.7,2.4,0.7,2.4,0.7,2.4,0.7],[1,1.5,0.5]],house)
+# hpc_house=dis(house)
 
-house= rem([40],house)
-# DRAW(house)
+house = unisci(6,[5,1,3],[[1,1,1,1,1],[1],[1,1.5,0.5]],house)
+hpc_house=dis(house)
+house = unisci(44,[3,1,3],[[0.7,1,0.7],[1],[1,1.5,0.5]],house)
+hpc_house=dis(house)
+house = unisci(22,[1,5,3],[[1],[3,.2,3,.2,3],[1,1.5,0.5]],house)
+hpc_house=dis(house)
+house = unisci(140,[1,3,1],[[1],[1,1,1],[1]],house)
+hpc_house=dis(house)
+house = unisci(145,[1,3,1],[[1],[1,1,1],[1]],house)
+hpc_house=dis(house)
 
-house = unisci(37,[3,1,3],[[4,8,4],[.3],[3,3,1.5]],house)
-hpc_house=dis(house)
-house = unisci(24,[1,5,3],[[.3],[3,.2,3.5,.2,2.5],[3,3,1.5]],house)
-hpc_house=dis(house)
-house = unisci(57,[1,3,1],[[.3],[1,1,1],[1]],house)
-hpc_house=dis(house)
-house = unisci(62,[1,3,1],[[.3],[1,1,1],[1]],house)
-house = unisci(3,[1,5,3],[[1],[1,1,0.5,1,1],[1,2,1]],house)
-house = unisci(6,[5,1,3],[[1,1,0.5,1,1],[1],[1,2,1]],house)
-house = unisci(12,[1,3,2],[[1],[1,1,1],[2,1]],house)
-house = unisci(96,[1,3,1],[[1],[1,1,1],[1]],house)
-house = unisci(23,[1,3,2],[[1],[1,1,1],[2,1]],house)
-house = unisci(103,[1,3,1],[[1],[1,1,1],[1]],house)
-house = unisci(27,[3,1,2],[[0.5,1,0.5],[1],[2,1]],house)
-house = unisci(28,[3,1,2],[[0.5,1,0.5],[1],[2,1]],house)
-house = unisci(102,[1,3,1],[[1],[1,1,1],[1]],house)
-house = unisci(103,[1,3,1],[[1],[1,1,1],[1]],house)
-hpc_house=dis(house)
-# VIEW(hpc_house)
+porte=[45,51,57,65,72,77,80]
+# porte=select(porte,house)
+finestre=[104,98,92,86,113,119,128,147,150]
+# finestre=select(finestre, house)
+empty=[7,38,39,41,34,35,37,29,33,24,26,28,27]
+# empty=select(empty, house)
+wall=sottrai(sottrai(sottrai(range(len(house[1])-1),empty),finestre),porte)
 
-house= rem([73,67,82,88,39,58,61, 99, 105, 109, 115,120, 123],house)
-DRAW(house)
+print(len([1,2,3]))
+
+# doors=house[0],porte
+# windows=house[0],finestre
+# empty=house[0],empty
+wall=house[0],select(wall,house)
+
+VIEW(STRUCT(MKPOLS((wall))))
+
--- 2014-05-16/python/exercise2.py
+++ 2014-05-16/python/exercise2.py
@@ -11,16 +11,38 @@ DRAW = COMP([VIEW,STRUCT,MKPOLS])
 def bezCurve(controlPoints):
 	return STRUCT(MKPOLS(larMap(larBezierCurve(controlPoints))(larDomain([32]))))
 
+def scale(larghezza, lunghezza, spessore, numero):
+	s=CUBOID([larghezza, lunghezza, spessore])
+	s=STRUCT([s,T([1,3])([larghezza,spessore])]*numero)
+	return s
+
+def sottrai(lista1, lista2):
+	lst=[]
+	for e in lista1:
+		if e not in lista2:
+			lst.append(e)
+	return lst
+
+
+
 def dis(pol):
 	hpc=SKEL_1(STRUCT(MKPOLS(pol)))
 	hpc = cellNumbering (pol,hpc)(range(len(pol[1])),CYAN,1)
 	return hpc
 
-def rem(list, pol):
-	toRemove = list
-	V,CV = pol
-	pol = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-	return pol
+""" rimuove una cella che va persa """
+# def rem(list, pol):
+# 	toRemove = list
+# 	V,CV = pol
+# 	pol = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+# 	return pol
+
+""" rimuove una cella e la inserisce in una lista """
+# def rem(toRemove, pol, addTo):
+# 	V,CV=pol
+# 	pol = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+# 	addTo=sottrai(CV,pol[1])
+# 	return (pol,addTo )
 
 def unisci(num,list1, list2, pol):
 	merge=num
@@ -28,51 +50,35 @@ def unisci(num,list1, list2, pol):
 	pol_final = diagram2cell(room,pol,merge)
 	return pol_final
 
-def scale(larghezza, lunghezza, spessore, numero):
-	s=CUBOID([larghezza, lunghezza, spessore])
-	s=STRUCT([s,T([1,3])([larghezza,spessore])]*numero)
-	return s
+def select(list, pol):
+	lst=[]
+	for i in list:
+		lst.append(pol[1][i])
+	return lst
 
 
+""" ********************************************************************************************************************************************** """
 
 house=assemblyDiagramInit([5,3,2])([[.3,5,.3,5.0,.3],[.3,10,.3],[.3,3]])
 
+""" liste celle utili """
 V,CV = house
-
-""" Creo il salone """
-hpc_house=dis(house)
-# VIEW(hpc_house)
-
-""" Identifico rimuovo il blocco del salone"""
-# toRemove = [9]
-# house = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-house= rem([9],house)
-# DRAW(house)
 
 """ Rinumero el celle """
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-""" Creo stanze piano terra destra """
-stanze=assemblyDiagramInit([3,3,1])([[2.5,0.3,2.7],[3.5,6.5],[1]])
-
-""" Mostro il blocco in cui innesto """
-toMerge=20
-# solidifico la stanza toMerge per vederla
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
+toMerge=21
 
 """ Creo nuove stanze e innesto"""
-stanze = assemblyDiagramInit([3,1,1])([[2.7,0.2,2.5],[.3],[2.2]])
+stanze = assemblyDiagramInit([3,1,1])([[2.5,0.2,2.3],[.3],[2.2]])
 house = diagram2cell(stanze,house,toMerge)
 
-#rinumero
+
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-toMerge=30
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
+toMerge=31
 
 """ Camera, Bagno, Camera """
 stanze = assemblyDiagramInit([3,5,1])([[1,0.3,4],[3,.2,3,.2,3],[2.2]])
@@ -80,19 +86,7 @@ house = diagram2cell(stanze,house,toMerge)
 
 hpc_house=dis(house)
 # VIEW(hpc_house)
-
-# toRemove = [40,42,44,30,35,32,33,34,39]
-# V,CV = house
-# house = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-house= rem([40,42,44,30,35,32,33,34,39], house)
-# DRAW(house)
-
-hpc_house=dis(house)
-# VIEW(hpc_house)
-
-toMerge=28
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
+toMerge=29
 
 """ Bagno, Cucina, ingresso """
 stanze = assemblyDiagramInit([1,5,1])([[5],[3,.2,3.5,.2,2.5],[2.2]])
@@ -101,73 +95,75 @@ house = diagram2cell(stanze,house,toMerge)
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-house=rem([35,37,39], house)
-# DRAW(house)
 
-""" porta"""
-toMerge=18
-cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
-# VIEW(STRUCT([hpc_house,cell]))
-
-house=unisci(toMerge,[3,1,1], [[14,1.1,13],[1],[1]], house)
-# stanze = assemblyDiagramInit([3,1,1])([[14,1.1,13],[1],[1]])
-# house = diagram2cell(stanze, house, toMerge)
-hpc_house=dis(house)
-# VIEW(hpc_house)
-
-toMerge=36
-# # solidifico la stanza toMerge per vederla
+""" porte """
+toMerge=19
 # cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
 # VIEW(STRUCT([hpc_house,cell]))
 
-house = unisci(36,[3,1,2],[[4,11,4],[.3],[2.2,.5]],house)
+house=unisci(toMerge,[3,1,1], [[14,1.1,13],[1],[1]], house)
 hpc_house=dis(house)
 # VIEW(hpc_house)
 
-house= rem([40],house)
-# DRAW(house)
+toMerge=49
+# # # solidifico la stanza toMerge per vederla
+# # cell = MKPOL([house[0],[[v+1 for v in  house[1][toMerge]]],None])
+# # VIEW(STRUCT([hpc_house,cell]))
+""" porta principale """
+house = unisci(toMerge,[3,1,2],[[0.75,1,0.75],[.3],[2,1]],house)
 
-house = unisci(37,[3,1,3],[[4,8,4],[.3],[3,3,1.5]],house)
-hpc_house=dis(house)
-house = unisci(24,[1,5,3],[[.3],[3,.2,3.5,.2,2.5],[3,3,1.5]],house)
-hpc_house=dis(house)
-house = unisci(57,[1,3,1],[[.3],[1,1,1],[1]],house)
-hpc_house=dis(house)
-# house = unisci(62,[1,3,1],[[.3],[1,1,1],[1]],house)
-# house = unisci(3,[1,5,3],[[1],[1,1,0.5,1,1],[1,2,1]],house)
-# house = unisci(6,[5,1,3],[[1,1,0.5,1,1],[1],[1,2,1]],house)
-# house = unisci(12,[1,3,2],[[1],[1,1,1],[2,1]],house)
-# house = unisci(96,[1,3,1],[[1],[1,1,1],[1]],house)
-# house = unisci(23,[1,3,2],[[1],[1,1,1],[2,1]],house)
-# house = unisci(103,[1,3,1],[[1],[1,1,1],[1]],house)
-# house = unisci(27,[3,1,2],[[0.5,1,0.5],[1],[2,1]],house)
-# house = unisci(28,[3,1,2],[[0.5,1,0.5],[1],[2,1]],house)
-# house = unisci(102,[1,3,1],[[1],[1,1,1],[1]],house)
-# house = unisci(103,[1,3,1],[[1],[1,1,1],[1]],house)
-# house = unisci(8,[5,1,3],[[1,1,0.5,1,1],[1],[1,2,1]],house)
-# house = unisci(44,[1,3,1],[[1],[1,1,1],[1]],house)
-# house = unisci(16,[5,1,3],[[1,1,1,1,0.8],[1],[1,1,1]],house)
+# house= rem([40],house)
+# # DRAW(house)
+""" porta ingresso/cucina """
+house = unisci(45,[3,1,2],[[0.75,1,0.75],[.3],[2,1]],house)
+# hpc_house=dis(house)
+""" porta ingresso/salone """
+house = unisci(15,[1,4,2],[[.3],[1,1,1,6.4],[2,1]],house)
+# hpc_house=dis(house)
+""" porta /ingresso/camera"""
+house = unisci(27,[1,4,2],[[.3],[1,1,1,6.4],[2,1]],house)
+# hpc_house=dis(house)
+""" porta camera1/bagno """
+house = unisci(38,[3,1,2],[[0.7,1.5,0.7],[1],[2,1]],house)
+# hpc_house=dis(house)
+""" Divisione parete camera cucina"""
+house = unisci(73,[1,2,1],[[1],[1,1],[1]],house)
+# hpc_house=dis(house)
+house = unisci(80,[1,3,1],[[1],[0.7,0.8,1.3],[1]],house)
+# hpc_house=dis(house)
+house = unisci(80,[1,3,1],[[1],[1.2,0.7,0.8],[1]],house)
+# hpc_house=dis(house)
+""" Finestre """
+house = unisci(3,[1,9,3],[[1],[0.7,2.4,0.7,2.4,0.7,2.4,0.7,2.4,0.7],[1,1.5,0.5]],house)
 # hpc_house=dis(house)
 
-house = unisci(62,[1,3,1],[[.3],[1,1,1],[1]],house)
-house = unisci(3,[1,5,3],[[1],[1,1,0.5,1,1],[1,2,1]],house)
-house = unisci(6,[5,1,3],[[1,1,0.5,1,1],[1],[1,2,1]],house)
-house = unisci(12,[1,3,2],[[1],[1,1,1],[2,1]],house)
-house = unisci(96,[1,3,1],[[1],[1,1,1],[1]],house)
-house = unisci(23,[1,3,2],[[1],[1,1,1],[2,1]],house)
-house = unisci(103,[1,3,1],[[1],[1,1,1],[1]],house)
-house = unisci(27,[3,1,2],[[0.5,1,0.5],[1],[2,1]],house)
-house = unisci(28,[3,1,2],[[0.5,1,0.5],[1],[2,1]],house)
-house = unisci(102,[1,3,1],[[1],[1,1,1],[1]],house)
-house = unisci(103,[1,3,1],[[1],[1,1,1],[1]],house)
+house = unisci(6,[5,1,3],[[1,1,1,1,1],[1],[1,1.5,0.5]],house)
 hpc_house=dis(house)
-# VIEW(hpc_house)
+house = unisci(44,[3,1,3],[[0.7,1,0.7],[1],[1,1.5,0.5]],house)
+hpc_house=dis(house)
+house = unisci(22,[1,5,3],[[1],[3,.2,3,.2,3],[1,1.5,0.5]],house)
+hpc_house=dis(house)
+house = unisci(140,[1,3,1],[[1],[1,1,1],[1]],house)
+hpc_house=dis(house)
+house = unisci(145,[1,3,1],[[1],[1,1,1],[1]],house)
+hpc_house=dis(house)
 
-house= rem([73,67,82,88,39,58,61, 99, 105, 109, 115,120, 123],house)
-# VIEW(hpc_house)
-# DRAW(house)
+porte=[45,51,57,65,72,77,80]
+# porte=select(porte,house)
+finestre=[104,98,92,86,113,119,128,147,150]
+# finestre=select(finestre, house)
+empty=[7,38,39,41,34,35,37,29,33,24,26,28,27]
+# empty=select(empty, house)
+wall=sottrai(sottrai(sottrai(range(len(house[1])-1),empty),finestre),porte)
 
+print(len([1,2,3]))
 
+# doors=house[0],porte
+# windows=house[0],finestre
+# empty=house[0],empty
+apartment=house[0],select(wall,house)
+# apartment=STRUCT(MKPOLS((apartment)))
+# VIEW(apartment)
 
 """ *************************************************************************************************************************** """
 """ *************************************************************************************************************************** """
@@ -175,27 +171,30 @@ house= rem([73,67,82,88,39,58,61, 99, 105, 109, 115,120, 123],house)
 """ *************************************************************************************************************************** """
 """ *************************************************************************************************************************** """
 
-house2 = larApply(t(2,2,0))(house)
-house2 = larApply(s(1,-1,1))(house2)
+apartment2 = larApply(t(2,-10,0))(apartment)
+apartment2 = larApply(s(1,-1,1))(apartment2)
+print("*************************************************",apartment2)
+# apartment2=STRUCT(MKPOLS((apartment2)))
+# VIEW(apartment2)
 
 # DRAW(house2)
 
-building=assemblyDiagramInit([1,2,4])([[10],[5,5],[3,3,3,3]])
+building=assemblyDiagramInit([1,2,4])([[10],[10,10],[3,3,3,3]])
 # DRAW(building)
 hpc_building=dis(building)
 	
 # VIEW(hpc_building)
 
-building = diagram2cell(house,building,0)
+building = diagram2cell(apartment,building,0)
 
-building = diagram2cell(house,building,0)
-building = diagram2cell(house,building,0)
-building = diagram2cell(house,building,0)
+building = diagram2cell(apartment,building,0)
+building = diagram2cell(apartment,building,0)
+building = diagram2cell(apartment,building,0)
 
-building = diagram2cell(house2,building,0)
-building = diagram2cell(house2,building,0)
-building = diagram2cell(house2,building,0)
-building = diagram2cell(house2,building,0)
+building = diagram2cell(apartment2,building,0)
+building = diagram2cell(apartment2,building,0)
+building = diagram2cell(apartment2,building,0)
+building = diagram2cell(apartment2,building,0)
 hpc_building=dis(building)
 	
 # VIEW(hpc_building)
@@ -205,8 +204,8 @@ hpc_building=dis(building)
 building_plasm=STRUCT(MKPOLS((building)))
 
 """ Scala """
-a=STRUCT([T([1,2])([4.75,10])(CUBOID([3,3,0.27]))])
-b=STRUCT([T(2)(-13)(a)])
+a=STRUCT([T([1,2])([4.75,20])(CUBOID([3,3,0.27]))])
+b=STRUCT([T(2)(-23)(a)])
 pian=STRUCT([a,b])
 pian=STRUCT([pian,T(3)(3)]*4)
 
@@ -214,11 +213,11 @@ pian=STRUCT([pian,T(3)(3)]*4)
 scala=STRUCT([T([1,2])([7.73,-2])(scale(0.35,1,0.27,6)),T([1,2,3])([9.83,-2,1.35])(R([1,2,])(PI)(scale(0.35,1,0.27,6)))])
 scala_inv=STRUCT([T([1,2,3])([12.5,-4,3])(R([1,2])(PI)(scala))])
 scala=STRUCT([T(3)(6)(scala), scala, scala_inv])
-scala2=STRUCT([T([1,2])([12.5,10])(R([1,2])(PI)(scala))])
+scala2=STRUCT([T([1,2])([12.5,20])(R([1,2])(PI)(scala))])
 
 # VIEW(STRUCT([scala, scala2]))
 """ Prato """
-terreno=STRUCT([T([2,3])([-5,-0.1])(CUBOID([20,20,0.1]))])
+terreno=STRUCT([T([1,2,3])([-5,-5,-0.1])(CUBOID([30,30,0.1]))])
 terreno=TEXTURE("../images/prato_02.jpg")(terreno)
 
 """ Lake """
@@ -246,11 +245,16 @@ bridge= T([1,2])([-1,4])(STRUCT([bridge]))
 bridge= TEXTURE("../images/wood_10.jpg")(bridge)
 
 """ Tetto """
-base=CUBOID([10,10])
-punta=MK([5,5,2])
-tetto=T(3)(12)(JOIN([base, punta]))
+C_tetto=[[0,0,12],[10,0,12],[10,20,12],[0,20,12],[0,0,12.5],[10,0,12.5],[10,20,12.5],[0,20,12.5],[5,10,15]]
+CV_tetto=[[0,1,2,3,4,5,6,7],[4,5,6,7,8]]
+
+tetto=T([1,2])([-0.5,-1])(S([1,2])([1.1,1.1])(STRUCT(MKPOLS((C_tetto,CV_tetto)))))
+tetto= TEXTURE("../images/tegole_03.jpg")(tetto)
+# base=CUBOID([10,10])
+# punta=MK([5,5,2])
+# tetto=T(3)(12)(JOIN([base, punta]))
 
 
 tree=T([1,2])([16,-2])(tree(9,0.4,3.0))
 
-VIEW(STRUCT([pian, building_plasm, tetto, scala, scala2,terreno, lake, bridge, tree]))
+VIEW(STRUCT([pian, building_plasm, scala, scala2,terreno, lake, bridge, tree, tetto]))
